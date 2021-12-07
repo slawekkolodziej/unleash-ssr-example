c2b9ffdfbdad558cf1623ae68a9083192d0922b0
Slawek Kolodziej <hfrntt@gmail.com>
2021-12-07 12:32:44 +0000

    Add getServerSideProps to force index page to use SSR

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import { useFlag } from '@unleash/proxy-client-react'
 
-const Page = () => {
+const Page = props => {
   const ssrDemoFlag = useFlag('ssr-demo')
 
+  console.log(
+    'random number, just to ensure we are using SSR: ',
+    props.randomNumber
+  )
   console.log('ssrDemoFlag value:', ssrDemoFlag)
 
   return <div>ssr-demo flag status: {ssrDemoFlag ? 'on' : 'off'}</div>
+}
+
+export const getServerSideProps = () => {
+  return {
+    props: {
+      randomNumber: Math.random()
+    }
+  }
 }
 
 export default Page
